8bc9026f7dae0302ec8c2b419e82b230ba7d4bf2
Naomi-Rhames <naomirhames2020@gmail.com>
2022-12-01 11:16:42 -0600

    Create serializer and create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+root 'pages#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :airlines, param: :slug
+      resources :reviews, only [:create, :destroy]
+    end
+  end
+  get '*path', to: 'pages#index', via: :all # route request that aren't for exisiting path predefined in our API back to our index path
 end
